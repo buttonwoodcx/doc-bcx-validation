3a344930575113ea8017cf6aa865a9e00e8d5177
Chunpeng Huo <huocp@me.com>
2018-05-17 13:48:02 +1000

    chore: show i18n json files

--- src/examples/e7-i18n/index.js
+++ src/examples/e7-i18n/index.js
@@ -1,6 +1,8 @@
 export class Index {
   sources = [
     'src/examples/e7-i18n/simple-form.js',
-    'src/examples/e7-i18n/simple-form.html'
+    'src/examples/e7-i18n/simple-form.html',
+    'locales/en/translation.json',
+    'locales/fr/translation.json'
   ];
 }
